b590a5dd4aa838c29a4b005baa3f00c1d82af775
Adithyan <Adithiruthiparambil12@gmail.com>
2025-01-09 15:45:34 +0530

    build: :rocket: Fixed Delploy Bugs

--- Back-End/server.ts
+++ Back-End/server.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import UserRoutes from "./Routes/UserRoutes";
 import express from "express";
+import cors from "cors"; 
 import { createBookIndex } from "./Middlewares/ElasticsearchMiddleware";
 const app = express();
 
@@ -13,9 +14,34 @@ const port = process.env.PORT || 5000;
 
 createBookIndex();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+const allowedOrigins = [
+    'https://www.tickethive.fun',
+    'https://ticket-hive-dusky.vercel.app/',
+    'http://localhost:3000'
+  ];
+  
+  app.use(cors({
+    origin: function(origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  }));
+  
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  
+  // Configure cookie settings for cross-origin
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+  
 
 app.use(express.static('Back-End/public')); 
 
